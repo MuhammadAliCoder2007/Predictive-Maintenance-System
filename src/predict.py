0e03b4cc7a379e9921104358f85ea4b1f027e7e8
Muhammad Ali <muhammad.ali.aktz@gmail.com>
2026-07-14 17:55:58 -0400

    Implement ML training and prediction pipeline

--- src/predict.py
+++ src/predict.py
@@ -1,2 +1,6 @@
-new_machine = [[95, 0.9, 130, 2200]]
+import joblib
+model = joblib.load("models/machine_failure_model.pkl")
+new_machine = [[95, 0.8, 130, 58, 2100]]
 prediction = model.predict(new_machine)
+
+print("Prediction:", prediction[0])
--- src/train_model.py
+++ src/train_model.py
@@ -1,1 +1,23 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import joblib
+
+df = pd.read_csv("data/machine_failure.csv")
+
+X = df.drop(columns=["machine_failure"])
+y = df["machine_failure"]
+
+# model = RandomForestClassifier()
+# model.fit(X, y)
+
+# print("Model trained successfully.")
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+print("Model trained successfully.")
+
+joblib.dump(model, "models/machine_failure_model.pkl")
+
