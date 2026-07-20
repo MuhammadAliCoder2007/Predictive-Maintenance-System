44403928751fb372ef6d5c6868eb18763e19a937
Muhammad Ali <muhammad.ali.aktz@gmail.com>
2026-07-20 19:42:02 -0400

    generated feature importances

--- src/visualize.py
+++ src/visualize.py
@@ -1,1 +1,18 @@
 #planning to visualize which features are most important for the model to predict the machine failure
+import joblib
+import pandas as pd
+
+model = joblib.load("models/machine_failure_model.pkl")
+
+feature_names = ["temperature","vibration","pressure","humidity","rpm"]
+
+
+importance = pd.DataFrame({
+    "feature": feature_names,
+    "importance": model.feature_importances_*100
+}).sort_values(by="importance",ascending=False)
+
+for _, row in importance.iterrows():
+    print(f"{row['feature']}: {row['importance']:.2f}%")
+
+
